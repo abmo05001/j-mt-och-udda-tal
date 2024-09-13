15ab115d32783ee6e351677197936fb890bffd85
abmo05001 <abmo05001@utb.vaxjo.se>
2024-09-13 13:48:32 +0200

    gissning

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int slumptal = random.Next(1, 101);  // Väljer ett slumptal mellan 1 och 100
+        int försök = 0;
+        int gissning = 0;
+
+        Console.WriteLine("Jag har valt ett tal mellan 1 och 100. Försök att gissa vilket det är!");
+
+        while (gissning != slumptal)
+        {
+            Console.Write("Din gissning: ");
+            gissning = int.Parse(Console.ReadLine());
+            försök++;
+
+            if (gissning < slumptal)
+            {
+                Console.WriteLine("För låg gissning. Försök igen!");
+            }
+            else if (gissning > slumptal)
+            {
+                Console.WriteLine("För hög gissning. Försök igen!");
+            }
+            else
+            {
+                Console.WriteLine($"Rätt tal! Du gissade rätt på {försök} försök.");
+            }
+        }
+    }
+}
+
